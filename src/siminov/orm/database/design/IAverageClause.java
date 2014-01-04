b45dcd2664f66fb40d0091f199cff261d075acee
Simply Innovative <pranavchauhan01@gmail.com>
2014-01-04 02:34:56 -0800

    Changes API parameter type to Object, this will allow to work on any data type.

--- src/siminov/orm/database/design/IAverageClause.java
+++ src/siminov/orm/database/design/IAverageClause.java
@@ -29,42 +29,43 @@ public interface IAverageClause {
 	 * @param value Value for which EQUAL TO (=) condition will be applied.
 	 * @return IAverage Interface.
 	 */
-	public IAverage equalTo(String value);
-
+	public IAverage equalTo(Object value);
+	
+	
 	/**
 	 * Used to specify NOT EQUAL TO (!=) condition.
 	 * @param value Value for which NOT EQUAL TO (=) condition will be applied.
 	 * @return IAverage Interface.
 	 */
-	public IAverage notEqualTo(String value);
+	public IAverage notEqualTo(Object value);
 	
 	/**
 	 * Used to specify GREATER THAN (>) condition.
 	 * @param value Value for while GREATER THAN (>) condition will be specified.
 	 * @return IAverage Interface.
 	 */
-	public IAverage greaterThan(String value);
+	public IAverage greaterThan(Object value);
 	
 	/**
 	 * Used to specify GREATER THAN EQUAL (>=) condition.
 	 * @param value Value for which GREATER THAN EQUAL (>=) condition will be specified.
 	 * @return IAverage Interface.
 	 */
-	public IAverage greaterThanEqual(String value);
+	public IAverage greaterThanEqual(Object value);
 	
 	/**
 	 * Used to specify LESS THAN (<) condition.
 	 * @param value Value for which LESS THAN (<) condition will be specified.
 	 * @return IAverage Interface.
 	 */
-	public IAverage lessThan(String value);
+	public IAverage lessThan(Object value);
 	
 	/**
 	 * Used to specify LESS THAN EQUAL (<=) condition.
 	 * @param value Value for which LESS THAN EQUAL (<=) condition will be specified.
 	 * @return IAverage Interface.
 	 */
-	public IAverage lessThanEqual(String value);
+	public IAverage lessThanEqual(Object value);
 	
 	/**
 	 * Used to specify BETWEEN condition.
@@ -72,20 +73,20 @@ public interface IAverageClause {
 	 * @param end End Range.
 	 * @return IAverage Interface.
 	 */
-	public IAverage between(String start, String end);
+	public IAverage between(Object start, Object end);
 	
 	/**
 	 * Used to specify LIKE condition.
 	 * @param like LIKE condition.
 	 * @return IAverage Interface.
 	 */
-	public IAverage like(String like);
+	public IAverage like(Object like);
 	
 	/**
 	 * Used to specify IN condition.
 	 * @param values Values for IN condition.
 	 * @return IAverage Interface.
 	 */
-	public IAverage in(String...values);
+	public IAverage in(Object...values);
 
 }
