cd2ec09c06835f4d7e2ddf9410a20c058ecf7e2a
Simply Innovative <pranavchauhan01@gmail.com>
2014-01-04 02:40:10 -0800

    Changes API parameter type to Object, this will allow to work on any data type.

--- src/siminov/orm/database/design/ITotalClause.java
+++ src/siminov/orm/database/design/ITotalClause.java
@@ -30,42 +30,43 @@ public interface ITotalClause {
 	 * @param value Value for which EQUAL TO (=) condition will be applied.
 	 * @return ITotal Interface.
 	 */
-	public ITotal equalTo(String value);
+	public ITotal equalTo(Object value);
 
+	
 	/**
 	 * Used to specify NOT EQUAL TO (!=) condition.
 	 * @param value Value for which NOT EQUAL TO (=) condition will be applied.
 	 * @return ITotal Interface.
 	 */
-	public ITotal notEqualTo(String value);
+	public ITotal notEqualTo(Object value);
 	
 	/**
 	 * Used to specify GREATER THAN (>) condition.
 	 * @param value Value for while GREATER THAN (>) condition will be specified.
 	 * @return ITotal Interface.
 	 */
-	public ITotal greaterThan(String value);
+	public ITotal greaterThan(Object value);
 	
 	/**
 	 * Used to specify GREATER THAN EQUAL (>=) condition.
 	 * @param value Value for which GREATER THAN EQUAL (>=) condition will be specified.
 	 * @return ITotal Interface.
 	 */
-	public ITotal greaterThanEqual(String value);
+	public ITotal greaterThanEqual(Object value);
 	
 	/**
 	 * Used to specify LESS THAN (<) condition.
 	 * @param value Value for which LESS THAN (<) condition will be specified.
 	 * @return ITotal Interface.
 	 */
-	public ITotal lessThan(String value);
+	public ITotal lessThan(Object value);
 	
 	/**
 	 * Used to specify LESS THAN EQUAL (<=) condition.
 	 * @param value Value for which LESS THAN EQUAL (<=) condition will be specified.
 	 * @return ITotal Interface.
 	 */
-	public ITotal lessThanEqual(String value);
+	public ITotal lessThanEqual(Object value);
 	
 	/**
 	 * Used to specify BETWEEN condition.
@@ -73,20 +74,20 @@ public interface ITotalClause {
 	 * @param end End Range.
 	 * @return ITotal Interface.
 	 */
-	public ITotal between(String start, String end);
+	public ITotal between(Object start, Object end);
 	
 	/**
 	 * Used to specify LIKE condition.
 	 * @param like LIKE condition.
 	 * @return ITotal Interface.
 	 */
-	public ITotal like(String like);
+	public ITotal like(Object like);
 	
 	/**
 	 * Used to specify IN condition.
 	 * @param values Values for IN condition.
 	 * @return ITotal Interface.
 	 */
-	public ITotal in(String...values);
+	public ITotal in(Object...values);
 	
 }
