b1953a63b1509fc9ef9b22ab824584ec88a70ab2
Simply Innovative <pranavchauhan01@gmail.com>
2014-01-04 02:39:16 -0800

    Changes API parameter type to Object, this will allow to work on any data type.

--- src/siminov/orm/database/design/ISumClause.java
+++ src/siminov/orm/database/design/ISumClause.java
@@ -30,42 +30,43 @@ public interface ISumClause {
 	 * @param value Value for which EQUAL TO (=) condition will be applied.
 	 * @return ISum Interface.
 	 */
-	public ISum equalTo(String value);
+	public ISum equalTo(Object value);
 
+	
 	/**
 	 * Used to specify NOT EQUAL TO (!=) condition.
 	 * @param value Value for which NOT EQUAL TO (=) condition will be applied.
 	 * @return ISum Interface.
 	 */
-	public ISum notEqualTo(String value);
+	public ISum notEqualTo(Object value);
 	
 	/**
 	 * Used to specify GREATER THAN (>) condition.
 	 * @param value Value for while GREATER THAN (>) condition will be specified.
 	 * @return ISum Interface.
 	 */
-	public ISum greaterThan(String value);
+	public ISum greaterThan(Object value);
 	
 	/**
 	 * Used to specify GREATER THAN EQUAL (>=) condition.
 	 * @param value Value for which GREATER THAN EQUAL (>=) condition will be specified.
 	 * @return ISum Interface.
 	 */
-	public ISum greaterThanEqual(String value);
+	public ISum greaterThanEqual(Object value);
 	
 	/**
 	 * Used to specify LESS THAN (<) condition.
 	 * @param value Value for which LESS THAN (<) condition will be specified.
 	 * @return ISum Interface.
 	 */
-	public ISum lessThan(String value);
+	public ISum lessThan(Object value);
 	
 	/**
 	 * Used to specify LESS THAN EQUAL (<=) condition.
 	 * @param value Value for which LESS THAN EQUAL (<=) condition will be specified.
 	 * @return ISum Interface.
 	 */
-	public ISum lessThanEqual(String value);
+	public ISum lessThanEqual(Object value);
 	
 	/**
 	 * Used to specify BETWEEN condition.
@@ -73,20 +74,20 @@ public interface ISumClause {
 	 * @param end End Range.
 	 * @return ISum Interface.
 	 */
-	public ISum between(String start, String end);
+	public ISum between(Object start, Object end);
 	
 	/**
 	 * Used to specify LIKE condition.
 	 * @param like LIKE condition.
 	 * @return ISum Interface.
 	 */
-	public ISum like(String like);
+	public ISum like(Object like);
 	
 	/**
 	 * Used to specify IN condition.
 	 * @param values Values for IN condition.
 	 * @return ISum Interface.
 	 */
-	public ISum in(String...values);
+	public ISum in(Object...values);
 
 }
